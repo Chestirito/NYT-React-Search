e67ae4af3d80136b2ec10e0baefa88478c163e9a
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-11-01 19:12:17 -0400

    no yarn

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+//const path = require("path");
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,12 +13,16 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// app.use(express.static(path.join(__dirname, "client", "build")))
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nyt-react");
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
